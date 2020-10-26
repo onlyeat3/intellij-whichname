12d8bfee7f5e2af2541d527c0773fabb30c10370
onlyeat3 <onlyeat3@gmail.com>
2020-10-26 17:48:51 +0800

    能够正常提示候选词的第一版

--- src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
+++ src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
@@ -1,26 +1,29 @@
 package io.github.onlyeat3.whichname;
 
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.textarea.TextComponentEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.ReadonlyStatusHandler;
-import com.intellij.util.io.HttpRequests;
-import com.intellij.util.io.RequestBuilder;
-import org.apache.http.client.HttpClient;
+import com.intellij.util.concurrency.AppExecutorUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class WhichNameAction extends AnAction {
     public static final ExecutorService EXECUTORS = Executors.newWorkStealingPool(2);
@@ -31,25 +34,37 @@ public class WhichNameAction extends AnAction {
         if (!isWriteable(editor.getProject(),editor.getDocument())) {
             return;
         }
+        editor.getSelectionModel().selectWordAtCaret(true);
         String selectedText = editor.getSelectionModel().getSelectedText(true);
-        EXECUTORS.submit(() -> {
-            try {
-                RequestBuilder requestBuilder = HttpRequests.request("https://devtuuls.tk/api/keyword_search");
-                requestBuilder.write(String.format("keyword=%s", URLEncoder.encode(selectedText)));
-                String responseJSON = requestBuilder
-                        .connect(HttpRequests.Request::readString);
-                LookupManager lookupManager = LookupManager.getInstance(project);
+        try {
+            List<String> list = AppExecutorUtil.getAppExecutorService().submit(new Callable<List<String>>() {
+                @Override
+                public List<String> call() throws Exception {
+                    Gson gson = new Gson();
+                    String body = HttpRequest.get(String.format("https://devtuuls.tk/api/keyword_search?keyword=%s", URLEncoder.encode(selectedText, "UTF-8")))
+                            .body();
+                    ArrayList<Map<String,String>> list = gson.fromJson(body, ArrayList.class);
+                    return list.stream()
+                            .map(e -> e.get("camel"))
+                            .filter(Objects::nonNull)
+                            .collect(Collectors.toList());
+                }
+            }).get();
+            LookupManager lookupManager = LookupManager.getInstance(project);
+            List<LookupElement> elements = new ArrayList<>();
+            for (String varName : list) {
                 @NotNull LookupElement items = new LookupElement() {
                     @Override
                     public @NotNull String getLookupString() {
-                        return responseJSON;
+                        return varName;
                     }
                 };
-                lookupManager.showLookup(editor,items);
-            } catch (IOException ex) {
-                ex.printStackTrace();
+                elements.add(items);
             }
-        });
+            lookupManager.showLookup(editor, elements.toArray(new LookupElement[]{}));
+        } catch (InterruptedException | ExecutionException interruptedException) {
+            interruptedException.printStackTrace();
+        }
     }
 
     public boolean isWriteable(Project project, Document document){
