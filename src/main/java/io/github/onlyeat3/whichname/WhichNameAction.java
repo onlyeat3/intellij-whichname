0a672ff22e919c8017e8ef4535fbbd69ea1e43c2
onlyeat3 <onlyeat3@gmail.com>
2020-10-28 16:38:57 +0800

    自动提供符合当前编程语言命名规则的变量名

--- src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
+++ src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
@@ -4,9 +4,11 @@ import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupManager;
+import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -15,10 +17,7 @@ import com.intellij.util.concurrency.AppExecutorUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -26,7 +25,22 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class WhichNameAction extends AnAction {
-    public static final ExecutorService EXECUTORS = Executors.newWorkStealingPool(2);
+    public static final Map<String, String> languageNamingRuleMap = new HashMap<>();
+    static {
+        languageNamingRuleMap.put("java","camel");
+        languageNamingRuleMap.put("objective-c","camel");
+        languageNamingRuleMap.put("swift","camel");
+        languageNamingRuleMap.put("sql","underline");
+        languageNamingRuleMap.put("go","underline");
+        languageNamingRuleMap.put("php","underline");
+        languageNamingRuleMap.put("python","underline");
+        languageNamingRuleMap.put("ruby","underline");
+        languageNamingRuleMap.put("javascript","camel");
+        languageNamingRuleMap.put("rust","underline");
+        languageNamingRuleMap.put("kotlin","camel");
+        languageNamingRuleMap.put("scala","camel");
+        languageNamingRuleMap.put("r","pascal");
+    }
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
@@ -34,21 +48,22 @@ public class WhichNameAction extends AnAction {
         if (!isWriteable(editor.getProject(),editor.getDocument())) {
             return;
         }
+        String languageName = LangDataKeys.LANGUAGE.getData(e.getDataContext()).getDisplayName().toLowerCase();
         editor.getSelectionModel().selectWordAtCaret(true);
         String selectedText = editor.getSelectionModel().getSelectedText(true);
         try {
-            List<String> list = AppExecutorUtil.getAppExecutorService().submit(new Callable<List<String>>() {
-                @Override
-                public List<String> call() throws Exception {
-                    Gson gson = new Gson();
-                    String body = HttpRequest.get(String.format("https://devtuuls.tk/api/lookup_var?word=%s", URLEncoder.encode(selectedText, "UTF-8")))
-                            .body();
-                    ArrayList<Map<String,String>> list = gson.fromJson(body, ArrayList.class);
-                    return list.stream()
-                            .map(e -> e.get("camel"))
-                            .filter(Objects::nonNull)
-                            .collect(Collectors.toList());
-                }
+            List<String> list = AppExecutorUtil.getAppExecutorService().submit(() -> {
+                Gson gson = new Gson();
+                String body = HttpRequest.get(String.format("https://devtuuls.tk/api/lookup_var?word=%s", URLEncoder.encode(selectedText, "UTF-8")))
+                        .body();
+                List<Map<String,String>> list1 = gson.fromJson(body, ArrayList.class);
+                return list1.stream()
+                        .map(e1 -> {
+                            String namingRule = languageNamingRuleMap.getOrDefault(languageName, "camel");
+                            return e1.get(namingRule);
+                        })
+                        .filter(Objects::nonNull)
+                        .collect(Collectors.toList());
             }).get();
             LookupManager lookupManager = LookupManager.getInstance(project);
             List<LookupElement> elements = new ArrayList<>();
