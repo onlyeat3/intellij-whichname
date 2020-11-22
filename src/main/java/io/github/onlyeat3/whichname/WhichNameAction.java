6e42a1bc4130fc510ff3b05ef57cec70972db2a1
onlyeat3 <onlyeat3@gmail.com>
2020-11-22 13:06:27 +0800

    修复不同语言命名规则适配不完整 单词差找逻辑改为模糊匹配，同时展示提示时，显示单词对应的中文解释

--- src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
+++ src/main/java/io/github/onlyeat3/whichname/WhichNameAction.java
@@ -3,6 +3,7 @@ package io.github.onlyeat3.whichname;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementPresentation;
 import com.intellij.codeInsight.lookup.LookupManager;
 import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.*;
@@ -48,36 +49,39 @@ public class WhichNameAction extends AnAction {
         String selectedText = editor.getSelectionModel().getSelectedText(true);
         String currentLanguageName = getCurrentLanguage(e.getDataContext());
         try {
-            List<String> list = AppExecutorUtil.getAppExecutorService().submit(new Callable<List<String>>() {
+            List<LookupElement> list = AppExecutorUtil.getAppExecutorService().submit(new Callable<List<LookupElement>>() {
                 @Override
-                public List<String> call() throws Exception {
+                public List<LookupElement> call() throws Exception {
                     Gson gson = new Gson();
                     String body = HttpRequest.get(String.format("https://devtuuls.tk/api/lookup_var?word=%s", URLEncoder.encode(selectedText, "UTF-8")))
                             .body();
-                    List<String> resultList = new ArrayList<>();
+
+                    List<LookupElement> elements = new ArrayList<>();
                     List<Map<String,String>> list1 = gson.fromJson(body, ArrayList.class);
                     for (Map<String, String> map : list1) {
                         String namingRule = languageNamingRuleMap.getOrDefault(currentLanguageName, "camel");
                         String varName = map.get(namingRule);
                         if (varName != null) {
-                            resultList.add(varName);
+                            @NotNull LookupElement lookupElement = new LookupElement() {
+                                @Override
+                                public void renderElement(LookupElementPresentation presentation) {
+                                    presentation.setTypeText(map.get("word"));
+                                    presentation.setItemText(varName);
+                                }
+
+                                @Override
+                                public @NotNull String getLookupString() {
+                                    return varName;
+                                }
+                            };
+                            elements.add(lookupElement);
                         }
                     }
-                    return resultList;
+                    return elements;
                 }
             }).get();
             LookupManager lookupManager = LookupManager.getInstance(project);
-            List<LookupElement> elements = new ArrayList<>();
-            for (String varName : list) {
-                @NotNull LookupElement items = new LookupElement() {
-                    @Override
-                    public @NotNull String getLookupString() {
-                        return varName;
-                    }
-                };
-                elements.add(items);
-            }
-            lookupManager.showLookup(editor, elements.toArray(new LookupElement[]{}));
+            lookupManager.showLookup(editor, list.toArray(new LookupElement[]{}));
         } catch (InterruptedException | ExecutionException interruptedException) {
             interruptedException.printStackTrace();
         }
@@ -89,8 +93,8 @@ public class WhichNameAction extends AnAction {
 
     public String getCurrentLanguage(DataContext dataContext){
         Language lang = LangDataKeys.LANGUAGE.getData(dataContext);
-        if (lang != null && lang.getBaseLanguage() != null) {
-            return lang.getBaseLanguage().getDisplayName().toLowerCase();
+        if (lang != null) {
+            return lang.getID().toLowerCase();
         }
         return null;
     }
